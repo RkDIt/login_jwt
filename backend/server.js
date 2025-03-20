fae3690e8648cbea2fcab44b659c1a791034e415
RkDit <ritikwins@gmail.com>
2025-03-20 14:00:52 +0530

    added authorization

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,8 @@ app.use(
   cors({
     origin: FRONTEND_URL,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(helmet());
