b98cdce90e847c690cd148996896c25b6267d90d
RkDit <ritikwins@gmail.com>
2025-03-20 13:35:06 +0530

    frontend url

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,20 @@ const authRoutes = require("./routes/authRoutes");
 const movieRoutes = require("./routes/movieRoutes");
 
 const app = express();
-
+const FRONTEND_URL = config.url;
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(helmet());
 app.use(morgan("dev"));
 app.use("/api/auth", authRoutes);
 app.use("/api/movie", movieRoutes);
-
 
 app.listen(config.port, () => console.log(`Server running on ${config.port}`));
